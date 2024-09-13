789a597530cc7d499f2d24b601960009e389efdc
Kliment Fis <kirilltrun2015@gmail.com>
2024-09-13 19:20:22 +0300

    Создана страница с созданием продукта, и вывода всех продуктов в таблицу

--- main/models.py
+++ main/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} | {self.description} | {self.price}"
